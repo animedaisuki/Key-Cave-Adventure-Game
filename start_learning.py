5037c5bfb0eba42efe712f210b03ba548e443be1
Wendy <xccxccxccb57@gmail.com>
2024-02-25 19:21:01 +1100

    bot now can solve simple task

--- start_learning.py
+++ start_learning.py
@@ -1,13 +1,13 @@
 from voyager import Voyager
 from bot import Bot
 
-openai_api_key = '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'
+openai_api_key = "YOUR OPENAI API KEY"
 
 bot = Bot("http://localhost:8000")
 
 voyager = Voyager(
     openai_api_key=openai_api_key,
-    ckpt_dir="/Users/amouyotsuha/Documents/Projects/Key-Cave-Adventure-Game/voyager/agents/ckpt",
+    ckpt_dir="/ckpt",
     bot=bot,
     dungeon_map="game1.txt"
 )
